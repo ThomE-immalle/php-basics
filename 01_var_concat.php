562d27f7d0addf29e937514f8d6c36e90d9c4b03
ThomE-immalle <thom.ernst@immalle.be>
2017-02-20 09:15:39 +0100

    oefening 2

--- 01_var_concat.php
+++ 01_var_concat.php
@@ -4,11 +4,11 @@
 // Plak de variabelen aan elkaar met de string-concatenatie-operator en zet het antwoord in $naam.
 // Toon het resultaat op de gerenderde html-pagina.
 
-$voornaam = ;
-$achternaam
+$voornaam = "Thom";
+$achternaam = "Ernst";
 
-$naam = 
-
+$naam = $voornaam . " " . $achternaam;
+echo $naam;
 
 
 ?>
