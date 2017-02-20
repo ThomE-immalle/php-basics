9d6b0a36adb3e07bfcb1bfc3b02dc67293588d4f
ThomE-immalle <thom.ernst@immalle.be>
2017-02-20 09:34:20 +0100

    oefening 4

--- 03_string_functions.php
+++ 03_string_functions.php
@@ -3,9 +3,9 @@
 $zin = "Binnenkort is het weeral lente";
 
 // Print af hoeveel woorden $zin bevat.
-
-
+echo str_word_count($zin);
+echo "<br>";
 // Vervang in $zin "lente" door "zomer" en druk af.
-
+echo str_replace("lente", "zomer", "$zin");
 
 ?>
