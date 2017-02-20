386b94d9514bd65e38a2b0013c0ea20312839a62
ThomE-immalle <thom.ernst@immalle.be>
2017-02-20 09:47:30 +0100

    oefening 6

--- 05_operators.php
+++ 05_operators.php
@@ -1,22 +1,22 @@
 <?php
 
 // Bereken de rest bij gehele deling van 10 door 3.
-
-
+echo 10%3;
+echo "<br>";
 // Bereken de 5e macht van 2.
-
-
+echo pow(2,5);
+echo "<br>";
 
 $var_a = "123";
 $var_b = 123;
 
 // Vervang de true in de if-voorwaarden zodat het klopt wat er staat:
 
-if(true) {
+if($var_a == $var_b) {
     echo '$var_a en $var_b zijn aan elkaar gelijk maar zijn niet van hetzelfde type.';
 }
 
-if(true) {
+if($var_a === $var_b) {
     echo '$var_a en $var_b zijn aan elkaar gelijk en van hetzelfde type.';
 }
 
